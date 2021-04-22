5c260b4ca83fd954e1af7163c42b823cba33ecbe
anildwarepo <51174510+anildwarepo@users.noreply.github.com>
2021-04-22 16:42:28 -0700

    Create recognize-document.py

---
+++ recognize-document.py
@@ -0,0 +1,42 @@
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.formrecognizer import FormRecognizerClient
+import os
+
+form_recognizer_client = FormRecognizerClient(endpoint="https://<>.cognitiveservices.azure.com/", credential=AzureKeyCredential("<key>"))
+
+
+
+
+
+def recognize_content(filename):
+    with open(filename, "rb") as source:
+        poller = form_recognizer_client.begin_recognize_content(form=source)
+    form_pages = poller.result()
+
+    aer_selection = []
+    #page = form_pages[0]
+    for page in form_pages:
+        for table in page.tables:
+            for cell in table.cells:
+                print("Cell[{}][{}] has text '{}'".format(
+                    cell.row_index,
+                    cell.column_index,
+                    cell.text
+                ))
+                if cell.field_elements:
+                    for element in cell.field_elements:
+                        if element.kind == "selectionMark":
+                            print("...Checkbox in cell is '{}' and has confidence of {}".format(
+                                element.state, element.confidence
+                            ))
+                            if element.state == "selected" and element.confidence > 0.8:
+                                aer_selection.append(cell.text)
+        print("nAER Selection: {}".format(aer_selection))
+
+
+for file in os.listdir("C:\\Users\\anildwa\\Downloads\\AER (2)\\AER"):
+    print("filename:" + file)
+    recognize_content(file)
+
+
+
